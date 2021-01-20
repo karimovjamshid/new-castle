ef55817f8db58f385796b070d028f0b9c6f2de37
karimovjamshid <67834450+karimovjamshid@users.noreply.github.com>
2021-01-20 16:46:05 +0500

    Version 1.00

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+// *************** Burger menu ***************  
+const menuBtn = document.querySelector('.burger');
+const miniNav = document.querySelector('.mini-nav');
+
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('burger__open');
+    miniNav.classList.toggle('active');
+})
+
+
+   // Smooth scroll
+$("[data-scroll]").on('click', function(event){
+  event.preventDefault();
+
+  let elemID = $(this).data('scroll');
+  let elemOffSet = $(elemID).offset().top;
+
+  $('html, body').animate({
+      scrollTop: elemOffSet - 88
+  });
+
+});
+
+// Sticky navbar
+    let header = $('#header');
+    let scrollPos = $(window).scrollTop();
+
+
+    $(window).on('scroll load resize', function(){
+        scrollPos = $(this).scrollTop();
+
+        if(scrollPos > 1){
+          header.addClass('fixed');
+        }else{
+          header.removeClass('fixed');
+        }
+    });
+
+$('.mini-nav__link').on('click', function(event){
+  event.preventDefault();
+  $('.mini-nav').removeClass('active');
+  $('.burger').removeClass('burger__open');
+})
+$('.mini-submenu__title').on('click', function(event){
+  event.preventDefault();
+  $('.mini-nav').removeClass('active');
+  $('.burger').removeClass('burger__open');
+})
+
+
+let miniNavEnglish = document.getElementById("miniNavEnglish");
+let miniSubmenu = document.querySelector(".mini-submenu");
+
+miniNavEnglish.addEventListener('click', function (e) {
+  e.preventDefault();
+  miniSubmenu.classList.toggle('active');
+})
