609b2ee15ef511a4535b7864dadcac8f28ca58c7
karimovjamshid <67834450+karimovjamshid@users.noreply.github.com>
2021-01-22 17:06:09 +0500

    Version 2.00

--- js/main.js
+++ js/main.js
@@ -1,48 +1,47 @@
-
 // *************** Burger menu ***************  
 const menuBtn = document.querySelector('.burger');
 const miniNav = document.querySelector('.mini-nav');
 
 menuBtn.addEventListener('click', function () {
-    menuBtn.classList.toggle('burger__open');
-    miniNav.classList.toggle('active');
+  menuBtn.classList.toggle('burger__open');
+  miniNav.classList.toggle('active');
 })
 
 
-   // Smooth scroll
-$("[data-scroll]").on('click', function(event){
+// Smooth scroll
+$("[data-scroll]").on('click', function (event) {
   event.preventDefault();
 
   let elemID = $(this).data('scroll');
   let elemOffSet = $(elemID).offset().top;
 
   $('html, body').animate({
-      scrollTop: elemOffSet - 88
+    scrollTop: elemOffSet - 88
   });
 
 });
 
 // Sticky navbar
-    let header = $('#header');
-    let scrollPos = $(window).scrollTop();
+let header = $('#header');
+let scrollPos = $(window).scrollTop();
 
 
-    $(window).on('scroll load resize', function(){
-        scrollPos = $(this).scrollTop();
+$(window).on('scroll load resize', function () {
+  scrollPos = $(this).scrollTop();
 
-        if(scrollPos > 1){
-          header.addClass('fixed');
-        }else{
-          header.removeClass('fixed');
-        }
-    });
+  if (scrollPos > 1) {
+    header.addClass('fixed');
+  } else {
+    header.removeClass('fixed');
+  }
+});
 
-$('.mini-nav__link').on('click', function(event){
+$('.mini-nav__link').on('click', function (event) {
   event.preventDefault();
   $('.mini-nav').removeClass('active');
   $('.burger').removeClass('burger__open');
 })
-$('.mini-submenu__title').on('click', function(event){
+$('.mini-submenu__title').on('click', function (event) {
   event.preventDefault();
   $('.mini-nav').removeClass('active');
   $('.burger').removeClass('burger__open');
@@ -56,3 +55,21 @@ miniNavEnglish.addEventListener('click', function (e) {
   e.preventDefault();
   miniSubmenu.classList.toggle('active');
 })
+
+
+// AOS 
+AOS.init({
+  duration: 1000,
+  easing: 'ease'
+});
+
+// To top
+let topBtn = $('#toTop');
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() > 30) {
+    topBtn.addClass('show');
+  } else {
+    topBtn.removeClass('show');
+
+  }
+});
